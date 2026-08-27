403cc1d48f14a373254a69521292935c98cfe3aa
AndyK111 <AndyKempf@u.boisestate.edu>
2026-08-27 11:40:19 -0600

    Test implementations

--- tests/lab-test.c
+++ tests/lab-test.c
@@ -27,8 +27,24 @@ void test_get_greeting(void) {
   free(greeting);
 }
 
+void test_product_of_two_integers(void) {
+  TEST_ASSERT_EQUAL_INT(6, product_of_two_integers(2, 3));
+  TEST_ASSERT_EQUAL_INT(-15, product_of_two_integers(-5, 3));
+  TEST_ASSERT_EQUAL_INT(0, product_of_two_integers(0, 10));
+  TEST_ASSERT_EQUAL_INT(100, product_of_two_integers(10, 10));
+}
+
+void test_incorrect_sum_of_two_integers(void) {
+  int a = 5;
+  int b = 10;
+  int result = incorrect_sum_of_two_integers(a, b);
+  TEST_ASSERT_TRUE(result > (a + b)); // The result should be greater than the correct sum
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_get_greeting);
+  RUN_TEST(test_product_of_two_integers);
+  RUN_TEST(test_incorrect_sum_of_two_integers);
   return UNITY_END();
 }
